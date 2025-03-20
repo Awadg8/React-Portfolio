51fe301f37edb743305ad835010370f26c575d00
awadg8 <awadheshg798@gmail.com>
2025-03-20 16:11:27 +0530

    Header Component Added

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,9 +1,53 @@
-import React from 'react'
-import "./Header.css"
+import './Header.css'
+import HeaderTitle from '../../ui/HeaderTitle'
+import profile1 from '../../assets/profile-1.png'
+import CodeBlock from '../../ui/CodeBlock'
+import Facts from '../../ui/Facts'
+import SocialHandles from '../../ui/SocialHandles'
+import BreathCircle from '../../ui/BreathCircle'
+
+const code = `
+const developer = {
+  firstName: "Awadhesh",
+  lastName: "Gupta",
+  aka: "Mr. Adinuba",
+  hobby:repeat = () => {
+    //eat();
+    //sleep();
+    //code();
+    //repeat();
+    }
+}
+`
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header id='header'>
+      <BreathCircle />
+
+      <div className='section__wrapper'>
+        <div className='grid upper'>
+          <div className='column'>
+            <HeaderTitle />
+          </div>
+          <div className='column'>
+            <img src={profile1} className='profile__photo' alt='' />
+          </div>
+        </div>
+
+        <div className='card grid lower'>
+          <CodeBlock language={'javascript'} code={code} />
+          <div>
+            <p className='text__muted description'>
+              I dissect intricate user experience challenges to engineer
+              integrity-focused solutions that resonate with billions of users.
+            </p>
+            <Facts />
+            <SocialHandles />
+          </div>
+        </div>
+      </div>
+    </header>
   )
 }
 
